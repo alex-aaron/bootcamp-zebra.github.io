86774c44af6502e03591e425bc721bcd4bba3155
Alex Aaron <alexfrancisaaron@gmail.com>
2022-08-15 20:28:08 +0000

    completed identity func

---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,133 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    return function(value){
+        if (value > base){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    return function(){
+
+    }
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+    return function(string){
+        if (string[0].toUpperCase() === startsWith.toUpperCase()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return the Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ */
+function modifyStrings(strings, modify) { // modify is intended to be a function
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+
+/*
+I: Function takes in an array of strings and a test function;
+O: Function should return true if all values return true when passed into the test function; 
+   function should return false if not all values return true when passed into the test function.
+C: N/A
+E: N/A
+*/
+
+function allStringsPass(strings, test) {
+    console.log(test);
+    // YOUR CODE BELOW HERE //
+    //takes an array of strings and a test function(function that returns true or false);
+    //if every string as we loop through passes test, should return true, otherwise false
+    for(var i = 0; i < strings.length; i++) {
+        if(test(strings[i]) === false) {
+            return false;
+        }
+    }
+    return true;
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
---
+++ studies/variables.js
@@ -0,0 +1,15 @@
+/**
+ * VARIABLES:
+ * 
+ * 0: Variables are named containers for data in javascript
+ * 
+ * 1: Variables must be declared with a declarative keyword. Depending on that keyword, variables can 
+ * be assigned or reassigned values over the course of the program.
+ * 
+ * 
+ */
+
+// 1 // Declaration and Assignment
+var name; // variable declared with var keyword
+
+name = 'Alex'; // variable name is now assigned a value of 'Alex'
